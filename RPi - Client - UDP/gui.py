cf509e2c4909a44c2410ff5f9524323ecb7fdbac
SpReeD <wackermann@nexn.systems>
2019-05-12 21:58:38 +0200

    Update

--- RPi - Client - UDP/gui.py
+++ RPi - Client - UDP/gui.py
@@ -24,14 +24,14 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.textinput import TextInput
 
 #Init Conffile
-clicksounds=1
+config_clicksounds=1
 
 confFilePath = os.getcwd() + '/config.conf'
 if os.path.isfile(confFilePath):
 	try:
 		config = configparser.ConfigParser()
 		config.read(confFilePath)
-		clicksounds = int(config.get('MAIN','clicksounds'))
+		config_clicksounds = int(config.get('MAIN','clicksounds'))
 		print('[+] conf loaded success...')
 	except Exception as e :
 		print(e)
@@ -45,8 +45,8 @@ else:
 		f.write('clicksounds=1\n')
 		f.close()
 
-#region UpperRightStuff
-class UpperRightIndicator(FloatLayout):
+#region TopStatusBar
+class TopStatusBar(FloatLayout):
 	lbl_dot = ObjectProperty()
 	lbl_time = ObjectProperty()
 
@@ -85,6 +85,36 @@ class UpperRightIndicator(FloatLayout):
 		self.lbl_time.text = now.strftime('%H:%M:%S')
 #endregion
 
+#region BottomStatusBar
+class BottomStatusBar(FloatLayout):
+	lbl_date = ObjectProperty()
+
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		#BlackBox behind
+		maincnv = Label(pos=(206,10), size=(148,25), size_hint=(None,None))
+		with maincnv.canvas.before:
+			Color(0,0,0,1)
+			Rectangle(pos=maincnv.pos, size=maincnv.size)
+		self.add_widget(maincnv)
+
+		lbl_box = Label(text='stardate:', pos=(212,8), size=(70,25), size_hint=(None,None), color=(0.99,0.61,0,1), font_name='fnt/lcarsgtj3.ttf', font_size='24sp')
+		lbl_box.bind(texture_size=lbl_box.setter('size'))
+		self.add_widget(lbl_box)
+
+		rndint = random.randint(10000,99999)
+		self.lbl_date = Label(text=str(rndint) + '.00', pos=(289,8), size=(100,25), size_hint=(None,None), color=(0.99,0.61,0,1), font_name='fnt/lcarsgtj3.ttf', font_size='24sp')
+		self.lbl_date.bind(texture_size=self.lbl_date.setter('size'))
+		self.add_widget(self.lbl_date)
+
+		self.timerDate(None)
+		Clock.schedule_interval(self.timerDate, 45)
+
+	def timerDate(self, instance):
+		rndint = random.randint(00,99)
+		self.lbl_date.text = self.lbl_date.text[:self.lbl_date.text.find('.')+1] + str("{:02d}".format(rndint))
+#endregion
+
 class ColorConversion():
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
@@ -98,57 +128,59 @@ class ColorConversion():
 
 		return (R,G,B,A)
 
-# region Buttons & LeftNavigation
 class MyButton(ButtonBehavior, Label):
 	def on_press(self):
-		if clicksounds:
-			self.PlayRndSound()
+		pass
 
-	#Play RandomSound
-	def PlayRndSound(self):
-		sounds = []
-		for sndfile in os.listdir('snd/clicks/'):
-			sounds.append('snd/clicks/' + str(sndfile))
-		rndint = random.randint(0,len(sounds)-1)
-		sound = SoundLoader.load(sounds[rndint])
-		sound.play()
-
-
-
-class ButtonLeftNavigation(FloatLayout):
-	def __init__(self, btnText, btnEnumPosition, btnEnumColor, btnEnumFunction, **kwargs):
+class MainLayout(FloatLayout):
+	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		bg = Image(source='img/bg.png', pos=(1,0), size_hint=(None,None), size=(800,480), id='background')
+		self.add_widget(bg)
+		txt_menu = Label(text='MENU', pos=(42,435), size=(35,18), size_hint=(None,None), color=(0,0,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='28 sp')
+		self.add_widget(txt_menu)
 
-		btn = MyButton(pos=self.EnumPosition(self, btnEnumPosition), size=(96,40), size_hint=(None,None))
-		with btn.canvas.before:
-			if btnEnumColor == 0: #Yellow
-				Color(1,1,0.2,1)
-			if btnEnumColor == 1: #Light yellow
-				Color(0.95,0.99,0.44,1)
-			if btnEnumColor == 2: #Blue
-				Color(0.6,0.8,1,1)
-			Rectangle(pos=btn.pos, size=btn.size)
-		btn_txt = Label(text=btnText, pos=(btn.pos[0]+2,btn.pos[1]-5), size=(50,13), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1))
-		btn_txt.bind(texture_size=btn_txt.setter('size'))
+		self.add_widget(TopStatusBar())
+		self.add_widget(BottomStatusBar())
+		self.LeftNavigation()
+		self.Page_Welcome()
 
-		btn.bind(on_press=self.EnumFunctions(self, btnEnumFunction))
-		btn_txt.bind(on_press=self.EnumFunctions(self, btnEnumFunction))
-
-		self.add_widget(btn)
-		self.add_widget(btn_txt)
+	def LeftNavigation(self):
+		self.ButtonCreation_LeftNavigation('welcome', 2, 0, 1, 2)
+		self.ButtonCreation_LeftNavigation('elite pips', 5, 1, 0, 1)
+		self.ButtonCreation_LeftNavigation('exit', 7, 2, 1, 0)
 
 	def EnumFunctions(self, instance, enum):
 		if enum == 0:
 			return self.exitapp
 		if enum == 1:
 			return self.elitedangerous_pips
+		if enum == 2:
+			return self.welcome_page
+
+	Pages = ['Welcome','ElitePIPS']
+
+	def clear_pages(self):
+		for x in range(5):
+			for child in self.children:
+				if child.id != None:
+					for page in self.Pages:
+						if page in child.id:
+							self.remove_widget(child)
+		Logger.info('PageFunction : Pages cleared')
+
+	def welcome_page(self, instance):
+		self.clear_pages()
+		self.Page_Welcome()
+		Logger.info('PageFunction : Pageswitch - Welcome')
 
 	def elitedangerous_pips(self, instance):
-		self.add_widget(ContentGrid_EliteDangerous(),-1)
-		#self.clear_widgets()
+		self.clear_pages()
+		self.Page_ElitePIPS()
+		Logger.info('PageFunction : Pageswitch - Elite PIPS')
 
 	def exitapp(self, instance):
-		App.get_running_app().stop()
+		Clock.schedule_interval(lambda a: App.get_running_app().stop(), 0.5)
 
 	def EnumPosition(self, instance, enum):
 		if enum == 0:
@@ -168,50 +200,67 @@ class ButtonLeftNavigation(FloatLayout):
 		if enum == 7:
 			return (11,56)
 
+	def ButtonCreation_LeftNavigation(self, btnText, btnEnumPosition, btnEnumColor, btnClickSounds=0, btnEnumFunction=None):
+		btn = MyButton(pos=self.EnumPosition(self, btnEnumPosition), size=(96,40), size_hint=(None,None))
+		with btn.canvas.before:
+			if btnEnumColor == 0: #Yellow
+				Color(1,1,0.2,1)
+			if btnEnumColor == 1: #Light yellow
+				Color(0.95,0.99,0.44,1)
+			if btnEnumColor == 2: #Blue
+				Color(0.6,0.8,1,1)
+			Rectangle(pos=btn.pos, size=btn.size)
+		btn_txt = Label(text=btnText, pos=(btn.pos[0]+2,btn.pos[1]-5), size=(50,13), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), id='btnLeftNavigation')
+		btn_txt.bind(texture_size=btn_txt.setter('size'))
 
-class Navigation(RelativeLayout):
-	def __init__(self, init_or_dont=1, **kwargs):
-		super().__init__(**kwargs)
+		if btnClickSounds != 0 and config_clicksounds != 0:
+			btn.bind(on_press=self.PlayRndSound)
+			btn_txt.bind(on_press=self.PlayRndSound)
 
-		self.add_widget(ButtonLeftNavigation('elite pips', 5, 1, 1))
-		self.add_widget(ButtonLeftNavigation('exit', 7, 2, 0))
-# endregion
+		if btnEnumFunction != None:
+			btn.bind(on_press=self.EnumFunctions(self, btnEnumFunction))
+			btn_txt.bind(on_press=self.EnumFunctions(self, btnEnumFunction))
 
-class BoundryRect(FloatLayout):
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-		boundryrect = Label(pos=(107,63), size=(675,354), size_hint=(None,None))
+		self.add_widget(btn)
+		self.add_widget(btn_txt)
 
-		with boundryrect.canvas.before:
-			Color(0, 0, 0, 1)
-			Rectangle(pos=boundryrect.pos, size=boundryrect.size)
-		self.add_widget(boundryrect)
+#region Sounds
+	#Play RandomSound
+	SoundPool_firstRun = True
+	SoundPool_Clicks = []
+	def fill_pool(self):
+		if self.SoundPool_firstRun == True:
+			for sndfile in os.listdir('snd/clicks/'):
+				self.SoundPool_Clicks.append('snd/clicks/' + str(sndfile))
+			self.SoundPool_firstRun = False
+			Logger.info('Sound Function : Initialized pool')
 
-class ContentGrid_WelcomeScreen(FloatLayout):
-	primary = None
+	def PlayRndSound(self, instance):
+		rndint = random.randint(0,len(self.SoundPool_Clicks)-1)
+		sound = SoundLoader.load(self.SoundPool_Clicks[rndint])
+		sound.play()
 
-	def __init__(self, **kwargs):
-		super(ContentGrid_WelcomeScreen, self).__init__(**kwargs)
-		self.add_widget(BoundryRect())
+#endregion
 
-		self.primary = Label(text='PRIMARY SYSTEMS TERMINAL', pos=(264,339), size=(362,37), size_hint=(None,None), color=(0.99,0.61,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='64sp')
-		self.add_widget(self.primary)
-		logo = Image(source='img/logo.png', pos=(227,182), size_hint=(None,None), size=(436,136))
+	def Page_Welcome(self):
+		id='Welcome'
+		primary = Label(text='PRIMARY SYSTEMS TERMINAL', pos=(264,339), size=(362,37), size_hint=(None,None), color=(0.99,0.61,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='64sp', id=id)
+		self.add_widget(primary)
+		logo = Image(source='img/logo.png', pos=(227,182), size_hint=(None,None), size=(436,136), id=id)
 		self.add_widget(logo)
-		access = Label(text='ACCESS GRANTED', pos=(338,93), size=(214,38), size_hint=(None,None), color=(0.99,0.61,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='64sp')
+		access = Label(text='ACCESS GRANTED', pos=(338,93), size=(214,38), size_hint=(None,None), color=(0.99,0.61,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='64sp', id=id)
 		self.add_widget(access)
+		#Init Functions
+		self.fill_pool()
 
-class ContentGrid_EliteDangerous(FloatLayout):
-	def __init__(self, **kwargs):
-		super(ContentGrid_EliteDangerous, self).__init__(**kwargs)
-		self.add_widget(BoundryRect())
-
+	def Page_ElitePIPS(self):
+		id='ElitePIPS'
 		#button reset
-		btn0 = MyButton(pos=(328,117), size=(233,46), size_hint=(None,None))
+		btn0 = MyButton(pos=(328,117), size=(233,46), size_hint=(None,None), id=id)
 		with btn0.canvas.before:
 			Color(1,1,0,1)
 			Rectangle(pos=btn0.pos, size=btn0.size)
-		btn0_txt = Label(text='[b]RESET[/b]', pos=(328,117), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True)
+		btn0_txt = Label(text='[b]RESET[/b]', pos=(328,117), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True, id=id)
 
 		btn0.bind(on_press=self.btn_reset)
 		btn0_txt.bind(on_press=self.btn_reset)
@@ -220,11 +269,11 @@ class ContentGrid_EliteDangerous(FloatLayout):
 		self.add_widget(btn0_txt)
 
 		#button SYSTEM
-		btn_system = MyButton(pos=(142,216), size=(233,46), size_hint=(None,None))
+		btn_system = MyButton(pos=(142,216), size=(233,46), size_hint=(None,None), id=id)
 		with btn_system.canvas.before:
 			Color(1,1,0,1)
 			Rectangle(pos=btn_system.pos, size=btn_system.size)
-		btn_system_txt = Label(text='[b]SYSTEM[/b]', pos=(142,216), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True)
+		btn_system_txt = Label(text='[b]SYSTEM[/b]', pos=(142,216), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True, id=id)
 
 		btn_system.bind(on_press=self.btn_system)
 		btn_system_txt.bind(on_press=self.btn_system)
@@ -233,11 +282,11 @@ class ContentGrid_EliteDangerous(FloatLayout):
 		self.add_widget(btn_system_txt)
 
 		#button WEAPONS
-		btn_weapons = MyButton(pos=(511,221), size=(233,46), size_hint=(None,None))
+		btn_weapons = MyButton(pos=(511,221), size=(233,46), size_hint=(None,None), id=id)
 		with btn_weapons.canvas.before:
 			Color(1,1,0,1)
 			Rectangle(pos=btn_weapons.pos, size=btn_weapons.size)
-		btn_weapons_txt = Label(text='[b]WEAPONS[/b]', pos=(511,221), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True)
+		btn_weapons_txt = Label(text='[b]WEAPONS[/b]', pos=(511,221), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True, id=id)
 
 		btn_weapons.bind(on_press=self.btn_weapons)
 		btn_weapons_txt.bind(on_press=self.btn_weapons)
@@ -246,11 +295,11 @@ class ContentGrid_EliteDangerous(FloatLayout):
 		self.add_widget(btn_weapons_txt)
 
 		#button speed
-		btn_speed = MyButton(pos=(328,319), size=(233,46), size_hint=(None,None))
+		btn_speed = MyButton(pos=(328,319), size=(233,46), size_hint=(None,None), id=id)
 		with btn_speed.canvas.before:
 			Color(1,1,0,1)
 			Rectangle(pos=btn_speed.pos, size=btn_speed.size)
-		btn_speed_txt = Label(text='[b]Speed[/b]', pos=(328,319), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True)
+		btn_speed_txt = Label(text='[b]Speed[/b]', pos=(328,319), size=(233,46), size_hint=(None,None), font_name='fnt/lcarsgtj3.ttf', font_size='24sp', color=(0,0,0,1), markup=True, id=id)
 
 		btn_speed.bind(on_press=self.btn_speed)
 		btn_speed_txt.bind(on_press=self.btn_speed)
@@ -259,43 +308,22 @@ class ContentGrid_EliteDangerous(FloatLayout):
 		self.add_widget(btn_speed_txt)
 
 	def btn_reset(self, instance):
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key down')
+		os.system('python "' + os.getcwd() + '/client_rpi.py" key {DOWN}')
 
 	def btn_system(self, instance):
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key down')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key left')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key left')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key left')
+		sequence = ['{DOWN}', '{LEFT}', '{LEFT}', '{LEFT}']
+		for key in sequence:
+			os.system('python "' + os.getcwd() + '/client_rpi.py" key ' + key)
 
 	def btn_weapons(self, instance):
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key down')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key right')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key right')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key right')
+		sequence = ['{DOWN}', '{RIGHT}', '{RIGHT}', '{RIGHT}']
+		for key in sequence:
+			os.system('python "' + os.getcwd() + '/client_rpi.py" key ' + key)
 
 	def btn_speed(self, instance):
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key down')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key up')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key up')
-		os.system('python "' + os.getcwd() + '/client_rpi.py" key up')
-
-class MainLayout(FloatLayout):
-	bg = ObjectProperty()
-
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-		self.bg = Image(source='img/bg.png', pos=(1,0), size_hint=(None,None), size=(800,480), id='background')
-		self.add_widget(self.bg)
-		txt_menu = Label(text='MENU', pos=(42,435), size=(35,18), size_hint=(None,None), color=(0,0,0,1), markup=True, font_name='fnt/lcarsgtj3.ttf', font_size='28 sp')
-		self.add_widget(txt_menu)
-
-		self.add_widget(Navigation())
-		self.add_widget(UpperRightIndicator())
-		self.add_widget(ContentGrid_WelcomeScreen())
-		#self.add_widget(ContentGrid_EliteDangerous())
-
-	def remove(self):
-		self.remove_widget(self.bg)
+		sequence = ['{DOWN}', '{UP}', '{UP}', '{UP}']
+		for key in sequence:
+			os.system('python "' + os.getcwd() + '/client_rpi.py" key ' + key)
 
 class MainApp(App):
 	def build(self):
@@ -303,7 +331,7 @@ class MainApp(App):
 		return MainLayout()
 
 	def on_stop(self):
-		Logger.critical('Good Bye')
+		Logger.critical('EXIT : Made with <3')
 
 if __name__ == '__main__':
 	#Window.show_cursor = False
--- Windows - Server/PY/server_pc.py
+++ Windows - Server/PY/server_pc.py
@@ -5,8 +5,10 @@ import datetime
 from subprocess import Popen
 from subprocess import PIPE
 import sys
-from pywinauto import Application
+from pywinauto import application
 from pywinauto import keyboard
+from pywinauto import mouse
+import win32api
 
 UDP_IP_ADDRESS = '192.168.1.137'
 UDP_PORT_NO = 15548
@@ -17,36 +19,38 @@ serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
 print('+ ------------------------- +\n| Server ' + str(version) + ' ready to work! |\n+ ------------------------- +\n\n')
 print('Waiting for commuication from cliens on port ' + str(UDP_PORT_NO) + '...')
 
-while True:
-	data, addr = serverSock.recvfrom(1024)
-	if(len(data) > 1):
-		pass
-	else:
-		continue
-	try:
-		cmd = data.decode().split("#")
-		#Data Work
-		if 'key' in cmd[0]:
-			#app = Application().connect(title_re="Elite*CLIENT*")
-			#app.type_keys('{' + cmd[1] + '}')
-			#keyboard.send_keys('{' + cmd[1] + '}')
-			keyboard.send_keys('{' + cmd[1].upper() + '}')
-		if 'hotkey' in cmd[0]:
-			hotkey(cmd[1],cmd[2])
-		if 'typewrite' in cmd[0]:
-			typewrite(cmd[1])
-		if 'program' in cmd[0]:
-			os.system(cmd[1])
-		# p = Popen(['/src/raspberry-remote/send',socket[0],socket[1],socket[2]], stdout=PIPE, stderr=PIPE)
-		# stdout, stderr = p.communicate()
-		# stdout = stdout.replace('\n',' --- ')
-		# stderr = stderr.replace('\n',' --- ')
+def ProcessCommand(cmd):
+	if 'key' in cmd[0]:
+		#mousex, mousey = win32api.GetCursorPos()
 
-		# if(len(stdout) <= 0):
-		# 	print('Some other error')
-		# else:
-		# 	print('Should work fine')
-	except Exception as e:
-		print('Error:' + str(e))
-	except KeyboardInterrupt:
-		print('User has killed me ;(')
+		#app = application.Application().connect(title_re='(?i).*untitled.*atOm')
+		#Form1 = app.window(title_re='(?i).*untitled.*atOm')
+		#Form1.type_keys('dasdas ' + cmd[1])
+
+		#mouse.move(coords=(mousex, mousey))
+
+		keyboard.send_keys(cmd[1])
+		print('Executed: ' + str(cmd[1]))
+	if 'hotkey' in cmd[0]:
+		hotkey(cmd[1],cmd[2])
+	if 'typewrite' in cmd[0]:
+		typewrite(cmd[1])
+	if 'program' in cmd[0]:
+		os.system(cmd[1])
+
+
+try:
+	while True:
+		data, addr = serverSock.recvfrom(1024)
+		if(len(data) > 1):
+			pass
+		else:
+			continue
+		try:
+			cmd = data.decode().split("#")
+			#Data Work
+			ProcessCommand(cmd)
+		except Exception as e:
+			print('Error:' + str(e))
+except KeyboardInterrupt:
+			print('User has killed me ;(')
